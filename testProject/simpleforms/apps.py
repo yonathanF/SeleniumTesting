42196523df453f05a7d8eeaae691fc8d745bb31c
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2017-11-25 02:03:22 -0500

    Subject line: Working register page
    Problem: On going work
    
    Solution: Still working. The register page is done. It takes a first name, last name, email, and password. It creates the user and if successful, redirects to /home. Still needs error checking (duplicates for example). The homepage will simple show a button a quote [will finish tomorrow]. These links were helpful in this work:
    
    https://docs.djangoproject.com/en/2.0/topics/auth/default/#user-objects
    https://docs.djangoproject.com/en/1.7/intro/tutorial01/
    
    [ticket: x] none

---
+++ testProject/simpleforms/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SimpleformsConfig(AppConfig):
+    name = 'simpleforms'
---
+++ testProject/simpleforms/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as signin
+
+def register(request):
+    if request.method=='GET':
+        return render(request, 'simpleforms/register.html')
+    else:
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        email=request.POST['email']
+        password=request.POST['password']
+
+        new_user=User.objects.create_user(username=email, first_name=first_name,
+                                          last_name=last_name, email=email, password=password)
+        user=authenticate(username=email, password=password)
+        if user is not None:
+            signin(request, user)
+            return redirect('/motivational/home/')
+        else:
+            return render(request, 'simpleforms/register.html')
+
+def login(request):
+    pass
+
+def home(request):
+    if request.user.is_authenticated:
+        return HttpResponse("worked")
+    else:
+        return HttpResponse("nah")

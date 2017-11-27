39143529fc6e2dd350dd976f48596cb15400800f
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2017-11-27 01:18:40 -0500

    tests are done!

--- testProject/simpleforms/regTestCaseSuccessful.py
+++ testProject/simpleforms/regTestCaseSuccessful.py
@@ -5,9 +5,31 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import unittest, time, re
 from django.test import LiveServerTestCase
+from simpleforms.models import Quote
 
+def fill_reg_form(base_url, driver):
+    driver.get(base_url + "/motivational/register")
+    driver.find_element_by_name("first_name").clear()
+    driver.find_element_by_name("first_name").send_keys("Atoshem")
+    driver.find_element_by_name("last_name").clear()
+    driver.find_element_by_name("last_name").send_keys("Gheb")
+    driver.find_element_by_name("email").clear()
+    driver.find_element_by_name("email").send_keys("123@gmail.com")
+    driver.find_element_by_name("password").clear()
+    driver.find_element_by_name("password").send_keys("123456")
+    driver.find_element_by_css_selector("button[type=\"submit\"]").click()
 
-class LoginTestCase(LiveServerTestCase):
+def fill_login_form(base_url, driver):
+    driver.get(base_url + "/motivational/login")
+    driver.find_element_by_name("email").clear()
+    driver.find_element_by_name("email").send_keys("123@gmail.com")
+    driver.find_element_by_name("password").clear()
+    driver.find_element_by_name("password").send_keys("123456")
+    driver.find_element_by_css_selector("button[type=\"submit\"]").click()        
+
+    
+
+class HomeTestCase(LiveServerTestCase):
     fixtures=['fixture.json']
     def setUp(self):
         self.driver = webdriver.Chrome()
@@ -16,42 +38,57 @@ class LoginTestCase(LiveServerTestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def fill_reg_form(self):
-        driver = self.driver
-        driver.get(self.base_url + "/motivational/register")
-        driver.find_element_by_name("first_name").clear()
-        driver.find_element_by_name("first_name").send_keys("Atoshem")
-        driver.find_element_by_name("last_name").clear()
-        driver.find_element_by_name("last_name").send_keys("Gheb")
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("123@gmail.com")
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("123456")
-        driver.find_element_by_css_selector("button[type=\"submit\"]").click()
+        fill_reg_form(self.base_url, self.driver)
 
+    def test_quotes_display(self):
+        driver = self.driver
+
+        fill_login_form(self.base_url, driver)
+        
+        quote=driver.find_element_by_name("quote")
+        self.assertFalse(quote is None)
+
+        quote=quote.text
+        quoteDB=Quote.objects.get(content=quote)
+        self.assertFalse(quoteDB is None)
+
+    def test_author_display(self):
+        driver = self.driver
+        fill_login_form(self.base_url, driver)
+
+        author=driver.find_element_by_name("author")
+        self.assertFalse(author is None)
+
+        author=author.text
+        authorDB=Quote.objects.get(author=author)
+        self.assertFalse(authorDB is None)
+
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+        
+class LoginTestCase(LiveServerTestCase): 
+    fixtures=['fixture.json']
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.base_url = self.live_server_url
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    
     def test_login_successful(self):
         driver = self.driver
-        self.fill_reg_form()
-        
-        driver.get(self.base_url + "/motivational/login")
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("123@gmail.com")
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("123456")
-        driver.find_element_by_css_selector("button[type=\"submit\"]").click()        
+        fill_reg_form(self.base_url, driver)        
+        fill_login_form(self.base_url, driver)
         
         self.assertTrue("Welcome Atoshem Gheb" in driver.page_source)
     
     def test_login_account_DNE(self):
         driver = self.driver
                 
-        driver.get(self.base_url + "/motivational/login")
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("123@gmail.com")
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("123456")
-        driver.find_element_by_css_selector("button[type=\"submit\"]").click()        
-        
+        fill_login_form(self.base_url, driver)
         self.assertTrue("Can't find that account. Try again please." in driver.page_source)
         
     def tearDown(self):
@@ -69,32 +106,19 @@ class RegiserTestCase(LiveServerTestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def fill_reg_form(self):
-        driver = self.driver
-        driver.get(self.base_url + "/motivational/register")
-        driver.find_element_by_name("first_name").clear()
-        driver.find_element_by_name("first_name").send_keys("Atoshem")
-        driver.find_element_by_name("last_name").clear()
-        driver.find_element_by_name("last_name").send_keys("Gheb")
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("123@gmail.com")
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("123456")
-        driver.find_element_by_css_selector("button[type=\"submit\"]").click()
-        
 
     def test_reg_successful(self):
         driver = self.driver
-        self.fill_reg_form()
+        fill_reg_form(self.base_url, driver)
         self.assertTrue("Welcome Atoshem Gheb" in driver.page_source)
 
     def test_reg_account_exists(self):
         driver = self.driver
-        self.fill_reg_form()
+        fill_reg_form(self.base_url, driver)
         
         driver.back()
 
-        self.fill_reg_form()
+        fill_reg_form(self.base_url, driver)
         self.assertTrue("Try again with a different email" in driver.page_source)
 
 
